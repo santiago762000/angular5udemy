96d4411026b26c4f8e48676a2a7dd5d46a17ff7e
Javier Panchi <javier.panchi@roames.com.au>
2017-12-23 20:39:59 +1000

    Lesson 19: Routing

---
+++ src/app/extramodules/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatToolbarModule, MatIconModule} from '@angular/material'
+
+@NgModule({
+  imports: [
+    CommonModule,MatButtonModule, MatToolbarModule, MatIconModule
+  ],
+  exports:[MatButtonModule, MatToolbarModule, MatIconModule],
+  declarations: []
+})
+export class MaterialModule { }

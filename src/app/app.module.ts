9867654dc26598fb9bb545c22793809469c1930c
Javier Panchi <javier.panchi@roames.com.au>
2017-12-23 20:29:06 +1000

    Lesson 18: Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {MaterialModule} from './extramodules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,13 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    MaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

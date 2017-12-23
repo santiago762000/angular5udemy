cf082c6c53e48d9f29808db8b871c07da7100e90
Javier Panchi <javier.panchi@roames.com.au>
2017-12-24 00:27:04 +1000

    Lesson26: Subscription

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../../services/github.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers:[GithubService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  username:string;
+  showSearchBox:Boolean;
+  constructor(private myService:GithubService) { }
 
   ngOnInit() {
+    this.resetAll();
+  }
+
+  resetAll(){
+    this.username="";
+    this.showSearchBox=true;
+  }
+
+  showContents(){
+  this.myService.getRepositories(this.username).subscribe(
+    data=>{console.log(data)
+      this.showSearchBox=false;
+    },
+    err=>{
+      this.showSearchBox=true;
+    }
+  )
   }
 
 }

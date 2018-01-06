e0149823bf4f594027b41a74739dac038b5b2022
Javier Panchi <javier.panchi@roames.com.au>
2018-01-06 14:41:15 +1000

    Changes to use stepper

--- src/app/extramodules/material/material.module.ts
+++ src/app/extramodules/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule} from '@angular/material'
+import {MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule, MatStepperModule} from '@angular/material'
 
 @NgModule({
-  imports: [CommonModule,MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule],
-  exports:[MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule],
+  imports: [CommonModule,MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule, MatStepperModule],
+  exports:[MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatInputModule,MatTabsModule,MatTableModule,MatPaginatorModule,MatSortModule,MatSnackBarModule, MatTooltipModule, MatExpansionModule, MatListModule, MatDialogModule, MatSelectModule, MatMenuModule, MatSidenavModule, MatStepperModule],
   declarations: []
 })
 export class MaterialModule { }
